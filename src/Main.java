373face42cd0cf2a6e70297062e8b39e7aa70a5b
sokolistener <holmse113@mail.ru>
2023-03-02 23:19:05 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,66 +3,37 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
+
+    static void createDirection (String dirPath, StringBuilder stringBuilder) {
+        File dir = new File(dirPath);
+        if (dir.mkdir())
+            stringBuilder.append(dir.getName() + " каталог создан\n");
+    }
+
+    static void createFile (String filePath,String fileName, StringBuilder stringBuilder) {
+        File dir = new File(filePath, fileName);
+        try {
+            if (dir.createNewFile())
+                stringBuilder.append(dir.getName() + " файл создан\n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
     public static void main(String[] args) {
         StringBuilder str1 = new StringBuilder();
+        String[] dirArray = new String[] {"D://Games/src", "D://Games/res", "D://Games/savegames", "D://Games/temp", "D://Games/src/main",
+                "D://Games/src/test", "D://Games/res/drawables", "D://Games/res/vectors", "D://Games/res/icons"};
+        String[] fileNames = new String[] {"Main.java", "Utils.java", "temp.txt"};
+        String[] filePath = new String[] {"D://Games/src/main", "D://Games/src/main", "D://Games/temp"};
 
-        File dir1 = new File("D://Games/src");
-        if (dir1.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-            //System.out.println(dir1.getName() + "каталог создан\n");
-
-        File dir2 = new File("D://Games/res");
-        if (dir2.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-        File dir3 = new File("D://Games/savegames");
-        if (dir3.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-        File dir4 = new File("D://Games/temp");
-        if (dir4.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-
-        File dir11 = new File("D://Games/src/main");
-        if (dir11.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-        File dir12 = new File("D://Games/src/test");
-        if (dir12.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-
-        File dir111 = new File("D://Games/src/main", "Main.java");
-        try {
-            if (dir111.createNewFile())
-                str1.append(dir111.getName() + " файл создан\n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        File dir112 = new File("D://Games/src/main","Utils.java");
-        try {
-            if (dir112.createNewFile())
-                str1.append(dir112.getName() + " файл создан\n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        for (String dir : dirArray) {
+            createDirection(dir, str1);
         }
 
-        File dir21 = new File("D://Games/res/drawables");
-        if (dir21.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-        File dir22 = new File("D://Games/res/vectors");
-        if (dir22.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-        File dir23 = new File("D://Games/res/icons");
-        if (dir23.mkdir())
-            str1.append(dir1.getName() + " каталог создан\n");
-
-
-        File dir41 = new File("D://Games/temp","temp.txt");
-        try {
-            if (dir41.createNewFile())
-                str1.append(dir41.getName() + " файл создан\n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        for (int i = 0; i < fileNames.length; i++) {
+            createFile(filePath[i],fileNames[i], str1);
         }
-
-
+//
         try (FileWriter writer  = new FileWriter("D://Games/temp/temp.txt")) {
             writer.write(str1.toString());
         } catch (IOException ex) {
